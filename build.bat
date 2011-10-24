f62c8d5c881de2e0fd54d08b63d4f46dbe190bfd
dnewcome <djn125@yahoo.com>
2011-10-23 22:45:39 -0700

    updated build system to build all configurations of crm query. This includes 3.5 framework version of crm4

---
+++ build.bat
@@ -0,0 +1,20 @@
+:: Usage:
+:: build 1.0.2
+
+:: setting point version on the commandline determines the name of the output folder.
+set msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+set PointVersion=1.0.0
+set TargetFrameworkVersion=v3.5
+
+set outpathprop=/property:OutputPath=..\release\%PointVersion%\%TargetFrameworkVersion%
+%msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM4\Debug /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
+%msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM4\Release /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
+
+:: note that we can't build 2011 against the 3.5 framework.
+:: %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM2011\Debug CrmQuery\CrmQuery2011.csproj
+:: %msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM2011\Release CrmQuery\CrmQuery2011.csproj
+
+%msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM4\Debug /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
+%msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM4\Release /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
+%msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM2011\Debug CrmQuery\CrmQuery2011.csproj
+%msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM2011\Release CrmQuery\CrmQuery2011.csproj
