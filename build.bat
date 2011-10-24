6c8457fa9187470f3106f639cdba7ea66739a1ec
dnewcome <djn125@yahoo.com>
2011-10-23 22:53:20 -0700

    fixed mistake in build output paths

--- build.bat
+++ build.bat
@@ -4,9 +4,8 @@
 :: setting point version on the commandline determines the name of the output folder.
 set msbuild=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
 set PointVersion=1.0.0
-set TargetFrameworkVersion=v3.5
 
-set outpathprop=/property:OutputPath=..\release\%PointVersion%\%TargetFrameworkVersion%
+set outpathprop=/property:OutputPath=..\release\%PointVersion%\3.5
 %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM4\Debug /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
 %msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM4\Release /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
 
@@ -14,6 +13,7 @@ set outpathprop=/property:OutputPath=..\release\%PointVersion%\%TargetFrameworkV
 :: %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM2011\Debug CrmQuery\CrmQuery2011.csproj
 :: %msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v3.5 %outpathprop%\CRM2011\Release CrmQuery\CrmQuery2011.csproj
 
+set outpathprop=/property:OutputPath=..\release\%PointVersion%\4.0
 %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM4\Debug /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
 %msbuild% /property:Configuration=Release /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM4\Release /p:DefineConstants="CRM4" CrmQuery\CrmQuery.csproj
 %msbuild% /property:Configuration=Debug /property:TargetFrameworkVersion=v4.0 %outpathprop%\CRM2011\Debug CrmQuery\CrmQuery2011.csproj
